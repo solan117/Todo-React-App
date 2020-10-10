34ebebd8294b20223685f7c4900d603079752589
Karan Solanki <karan.solanki69@gmail.com>
2020-10-09 22:25:17 -0400

    Updates

---
+++ precache-manifest.491c5c90ae8ef714140e03025d416398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242ad4daffa571a10bfe0cac875a8f8c",
+    "url": "/Todo-React-App/index.html"
+  },
+  {
+    "revision": "ebce2c359cfb7044b7c6",
+    "url": "/Todo-React-App/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "bc32e9450d867891c176",
+    "url": "/Todo-React-App/static/js/2.4a4c7e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-React-App/static/js/2.4a4c7e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebce2c359cfb7044b7c6",
+    "url": "/Todo-React-App/static/js/main.d9358da1.chunk.js"
+  },
+  {
+    "revision": "5be4bc9f2e3f2b5f0e40",
+    "url": "/Todo-React-App/static/js/runtime-main.270b3318.js"
+  }
+]);
